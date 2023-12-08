42171354312c303273cad408edcd0949f3380ff1
Kaustubh Mayekar <67229974+Kaustubh01@users.noreply.github.com>
2023-12-09 01:46:05 +0530

    report template started

--- report.py
+++ report.py
@@ -1,32 +1,31 @@
 from flask import Blueprint, render_template, url_for, redirect, session,request, jsonify
 from database import *
 import datetime
+import matplotlib.pyplot as plt
+from io import BytesIO
+import base64
 
 report_bp = Blueprint('report',__name__)
 
+def generate_pie_chart():
+    labels = ['Label 1', 'Label 2', 'Label 3']
+    values = [30, 50, 20]
 
+    fig, ax = plt.subplots()
+    ax.pie(values, labels=labels, autopct='%1.1f%%', startangle=90)
+    ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle
 
-@report_bp.route('/data')
-def data():
+    # Save the plot to a BytesIO object
+    image_stream = BytesIO()
+    plt.savefig(image_stream, format='png')
+    image_stream.seek(0)
 
-    years = {}
-    internships = get_all_internships()
-    for internship in internships:
-        if internship.status == 'completed':
-            year = internship.year
-            if year in years:
-                years[year] +=1
-            else:
-                years[year] = 1
-    
-    print(years)
+    # Encode the image as base64
+    image_base64 = base64.b64encode(image_stream.read()).decode('utf-8')
 
-    company_data = {
-    "years": list(years.keys()),
-    "students": list(years.values()),
-    # "color": ["red", "green", "blue", "orange"]
-    }
-    return jsonify(company_data)
+    plt.close()  # Close the plot to free up resources
+
+    return image_base64
 
 @report_bp.route('/report-nav')
 def report_nav():
@@ -35,16 +34,7 @@ def report_nav():
 @report_bp.route('/last-three-years')
 def last_three_years():
     internships = get_all_internships()
+    chart_image = generate_pie_chart()
 
-    data = []  # Create an empty dictionary
-    for internship in internships:
-        data.append({
-        'id' : internship.internship_id,
-        'year' : internship.year,
-        'name':get_student_name(internship.prn),
-        'class' :internship.std_class,
-        'organization' : internship.organization
-        })
-    print(data)
 
-    return render_template('last-three-years.html', data = data)
+    return render_template('year_end_summary.html', chart_image=chart_image)
